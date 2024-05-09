d8846858ba373438a034199ff4eb9900fcbfee35
AV-MMI <villalobos.aaron@hotmail.com>
2024-05-08 20:47:08 -0400

    added tailwind, ts, react, vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+    <div className=""><h1 className="text-red-800">A lsot</h1></div>
+    </>
+  )
+}
+
+export default App
